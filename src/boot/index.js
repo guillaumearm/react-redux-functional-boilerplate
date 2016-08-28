02c3863b4657e81bccd2a3d21f0fed609bfecd1d
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-08-28 14:46:14 +0200

    boot : add boot.prod.js and boot.dev.js

--- src/boot/index.js
+++ src/boot/index.js
@@ -1,1 +1,7 @@
 console.log(`Running in ${process.env.NODE_ENV} mode.`)
+
+if (__PRODUCTION__) {
+    module.exports = require('./boot.prod')
+} else {
+    module.exports = require('./boot.dev')
+}
