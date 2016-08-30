6404f1b9f36101b9f45ad013a00e8c86c735bfde
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-08-30 21:32:18 +0200

    boot : add comment.

--- src/boot/index.js
+++ src/boot/index.js
@@ -3,6 +3,7 @@ console.log(`Running in ${process.env.NODE_ENV} mode.`)
 // load styles
 import 'styles'
 
+// Specific boot modes.
 if (__PRODUCTION__) {
     module.exports = require('./boot.prod')
 } else {
