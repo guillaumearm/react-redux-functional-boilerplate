b5e37048d6f98e2a77f6e7804a7d8b59af4322c3
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-12-21 22:21:06 +0100

    Add jest : resolve #2

---
+++ __tests__/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+import * as App from 'App';
+
+describe('react-redux-functional-boilerplate', () => {
+    describe('exports', () => {
+        it('has a src/App component', () => {
+            expect(App).toBeDefined();
+        });
+    });
+});
