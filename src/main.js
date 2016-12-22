06ab490a83d8696bf623cf713b4962b62e62f8fb
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-12-22 01:07:16 +0100

    Add dev server + electron support
    - add server.js
    - add dev:electron script
    - add dev:prod:electron script
    - add webpack-target-electron-renderer
    - add __ELECTRON__ webpack define

---
+++ src/main.js
@@ -0,0 +1,48 @@
+/* ---------- Electron init ------------------------------------------------- */
+import { app, BrowserWindow } from 'electron'
+/* ---------- Requires ------------------------------------------------------ */
+import { host, port } from '../scripts/config'
+import path from 'path'
+/* ---------- Refs for garbage collection ----------------------------------- */
+let window
+/* -------------------------------------------------------------------------- */
+
+if (process.env.NODE_ENV === 'development')
+    require('electron-reload')(__dirname)
+
+function createBrowserWindow () {
+    // Init Window
+    window = new BrowserWindow({
+        width:    1024,
+        height:   700,
+        frame:    true,
+    })
+
+    // Load template file
+    window.loadURL(`http://${host}:${port}`)
+
+    // Open the DevTools.
+    if (process.env.NODE_ENV === 'development')
+        window.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    window.on('closed', () => {
+        process.exit(0)
+        window = null
+    })
+}
+
+app.on('ready', createBrowserWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (window === null) {
+        createBrowserWindow()
+    }
+})
