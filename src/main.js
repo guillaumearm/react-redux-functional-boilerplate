fae92e4068fcea848fff57c8e5dce38bcbbedcc1
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-12-23 21:13:05 +0100

    Add react developer tools and redux devtools
    - install electron-devtools-installer
    - add setTimeout and setImmediate in eslintrc globals

--- src/main.js
+++ src/main.js
@@ -4,11 +4,37 @@ import { app, BrowserWindow } from 'electron'
 import { host, port } from '../scripts/config'
 import { isDev } from '../scripts/env'
 import path from 'path'
+/* ----------- Devtools ------------------------------------------------------ */
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } from 'electron-devtools-installer';
 /* ---------- Refs for garbage collection ----------------------------------- */
 let window
+let firstTime = true;
 /* -------------------------------------------------------------------------- */
 
+const installDevTools = () => Promise.all([
+    installExtension(REACT_DEVELOPER_TOOLS),
+    installExtension(REDUX_DEVTOOLS),
+])
+
+const openDevTools = () => {
+    setTimeout(() => {
+        window.webContents.openDevTools();
+        firstTime = false;
+    }, firstTime ? 3000 : 0)
+};
+
+function setDevelopmentMode() {
+    installDevTools()
+        .then((names) => {
+            console.log(`Added Extension: ${names}`);
+            openDevTools();
+        })
+        .catch((err) => console.log('An error occurred: ', err));
+}
+
 function createBrowserWindow () {
+    if (isDev()) setDevelopmentMode();
+
     // Init Window
     window = new BrowserWindow({
         width:    1024,
@@ -18,10 +44,6 @@ function createBrowserWindow () {
 
     // Load template file
     window.loadURL(`http://${host}:${port}`)
-
-    // Open the DevTools.
-    if (isDev())
-        window.webContents.openDevTools()
 
     // Emitted when the window is closed.
     window.on('closed', () => {
